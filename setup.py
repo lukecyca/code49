b9356e9d3c0770511816d2c73aea3e5a2d8e16d2
Luke Cyca <me@lukecyca.com>
2014-01-27 15:14:59 -0800

    Packaged for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="code49",
+    version="0.1",
+    description="Code 49 barcode generation",
+    author="Luke Cyca",
+    author_email="me@lukecyca.com",
+    license="MIT",
+    keywords="barcode stacked 2d code49",
+    url="http://github.com/lukecyca/code49",
+    packages=["code49"],
+)
